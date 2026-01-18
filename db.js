e3a4161be3bbd19b8235c874d90e58915b0af139
Kaider O <kaider-holger.oper@tktk.ee>
2026-01-18 03:13:33 +0200

    week 12

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const { Pool } = require('pg');
-const itemsPool = new Pool({
+const itemsPool = new Pool ({
     connectionString: process.env.DBconnectionString,
     ssl: {
         rejectUnauthorized: false
